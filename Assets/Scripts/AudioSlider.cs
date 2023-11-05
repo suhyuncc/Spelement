a5c91eb75cfa18c8d6a47bd82c2dc9fa59dae2cb
owl2lwo12 <owl2lwo12@ajou.ac.kr>
2023-11-05 13:25:06 +0900

    put: toggle, text on Option UI

--- Assets/Scripts/AudioSlider.cs
+++ Assets/Scripts/AudioSlider.cs
@@ -1,38 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-
+//gh
 public class AudioSlider : MonoBehaviour
 {
     [SerializeField] private AudioMixer Mixer;
     [SerializeField] private Slider Master;
     [SerializeField] private Slider BGM;
     [SerializeField] private Slider Effect;
-
+    [SerializeField] private Toggle masterT;
+    [SerializeField] private Toggle BGMT;
+    [SerializeField] private Toggle EffectT;
+    [SerializeField] private TMP_Text masterText;
+    [SerializeField] private TMP_Text BGMText;
+    [SerializeField] private TMP_Text EffectText;
 
     public void SetMasterVolume ()
     {
         float sound = Master.value;
-
-        if (sound == 0) Mixer.SetFloat("Master", -80);
+        masterText.text = (int)sound + "%";
+        if (masterT.isOn)
+        {
+            Mixer.SetFloat("Master", -80);
+        }
         else
         {
-            sound = -40 + sound * 4 / 10;
-            Mixer.SetFloat("Master", sound);
+            if (sound == 0) Mixer.SetFloat("Master", -80);
+            else
+            {
+                sound = -40 + sound * 4 / 10;
+                Mixer.SetFloat("Master", sound);
+            }
         }
     }
 
     public void SetBGMVolume()
     {
         float sound = BGM.value;
-
-        if (sound == 0) Mixer.SetFloat("Background", -80);
+        BGMText.text = (int)sound + "%";
+        if (BGMT.isOn)
+        {
+            Mixer.SetFloat("Background", -80);
+        }
         else
         {
-            sound = -40 + sound * 4 / 10;
-            Mixer.SetFloat("Background", sound);
+            if (sound == 0) Mixer.SetFloat("Background", -80);
+            else
+            {
+                sound = -40 + sound * 4 / 10;
+                Mixer.SetFloat("Background", sound);
+            }
         }
     }
 
@@ -40,12 +60,77 @@ public class AudioSlider : MonoBehaviour
     {
 
         float sound = Effect.value;
-
-        if (sound == 0) Mixer.SetFloat("Effect", -80);
+        EffectText.text = (int)sound + "%";
+        if (EffectT.isOn)
+        {
+            Mixer.SetFloat("Effect", -80);
+        }
         else
         {
-            sound = -40 + sound * 4 / 10;
-            Mixer.SetFloat("Effect", sound);
+            if (sound == 0) Mixer.SetFloat("Effect", -80);
+            else
+            {
+                sound = -40 + sound * 4 / 10;
+                Mixer.SetFloat("Effect", sound);
+            }
+        }
+    }
+
+    public void MasterToggleClicked()
+    {
+        if(masterT.isOn)
+        {
+            masterText.color = new Color(1, 1, 1, 0.1f);
+            Mixer.SetFloat("Master", -80);
+        }
+        else
+        {
+            masterText.color = new Color(1, 1, 1, 1f);
+            float sound = Master.value * 4 / 10;
+            if (sound == 0) Mixer.SetFloat("Master", -80);
+            else
+            {
+                sound -= 40;
+                Mixer.SetFloat("Master", sound);
+            }
+        }
+    }
+    public void BackToggleClicked()
+    {
+        if (BGMT.isOn)
+        {
+            BGMText.color = new Color(1, 1, 1, 0.1f);
+            Mixer.SetFloat("Background", -80);
+        }
+        else
+        {
+            BGMText.color = new Color(1, 1, 1, 1f);
+            float sound = BGM.value * 4 / 10;
+            if (sound == 0) Mixer.SetFloat("Background", -80);
+            else
+            {
+                sound -= 40;
+                Mixer.SetFloat("Background", sound);
+            }
+        }
+    }
+    public void EffectToggleClicked()
+    {
+        if (EffectT.isOn)
+        {
+            EffectText.color = new Color(1, 1, 1, 0.1f);
+            Mixer.SetFloat("Effect", -80);
+        }
+        else
+        {
+            EffectText.color = new Color(1, 1, 1, 1f);
+            float sound = Effect.value * 4 / 10;
+            if (sound == 0) Mixer.SetFloat("Effect", -80);
+            else
+            {
+                sound -= 40;
+                Mixer.SetFloat("Effect", sound);
+            }
         }
     }
 }
