1446e1f031ccf76bac015a4961cf43f46b014bb7
owl2lwo12 <owl2lwo12@ajou.ac.kr>
2023-12-04 16:41:46 +0900

    put : Credit

--- Assets/Scripts/startsceneManager.cs
+++ Assets/Scripts/startsceneManager.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class startsceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Startbtn()
     {
         GameObject gm = GameObject.Find("GameManager");
